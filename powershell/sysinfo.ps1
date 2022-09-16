51ba4ffcacac2f53afbf25d6c71a485ecc7b8078
nickromeo <romeonj@mail.uc.edu>
2022-09-16 18:45:23 -0400

    powershell files

--- powershell/sysinfo.ps1
+++ powershell/sysinfo.ps1
@@ -1,3 +1,14 @@
-﻿$IP = getIP
-Write-Host("this machines IP is $IP")
-Write-Host("this machine IP is {0}" -f $IP)
+﻿function getIP { 
+(Get-NetIPAddress).ipv4address | Select-String "192"
+}
+function getHost{ hostname
+}
+function getVer{ $host.Version 
+}
+function date{ Get-Date }
+
+$IP = getIP
+$NAME = getHost
+$VER = getVer
+$DATE = date
+$Body = "this machines IP is $IP, this machine name is $NAME, this machine's powershell verion is $VER, Todays date is $DATE"
